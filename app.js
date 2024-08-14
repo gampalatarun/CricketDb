bf4fab5f7a1a1ad3c525f208918004ee67a7ce35
tarungampala <48049736+gampalatarun@users.noreply.github.com>
2024-08-14 18:12:02 +0530

    API 3 AND API 4  AND api 5 are added

--- app.js
+++ app.js
@@ -68,13 +68,48 @@ app.get('/players/', async (request, response) => {
     ),
   )
 })
-module.exports = app
 
 //API2
 
 app.get('/players/:playerId/', async (request, response) => {
-  const {playerId} = request.params;
+  const {playerId} = request.params
   const getPlayerQuery = ` SELECT * FROM player_details WHERE player_id=${playerId};`
   const getPlayerId = await db.get(getPlayerQuery)
   response.send(ConvertsnaketoCamelCaseofPlayerDetails(getPlayerId))
 })
+
+//API3
+app.put('/players/:playerId/', async (request, response) => {
+  const {playerId} = request.params
+  const {playerName} = request.body
+  const updateQuery = `UPDATE player_details SET
+  player_name='${playerName}' WHERE player_id=${playerId};`
+
+  await db.run(updateQuery)
+
+  response.send('Player Details Updated')
+})
+
+//API 4
+
+app.get('/matches/:matchId/', async (request, response) => {
+  const {matchId} = request.params
+  const MatchQuery = `SELECT * FROM match_details WHERE match_id=${matchId};`
+  const AllmatchDetails = await db.get(MatchQuery)
+
+  response.send(ConvertsnaketoCamelCaseofMatchDetails(AllmatchDetails))
+})
+
+//API 5
+app.get('/players/:playerId/matches', async (request, response) => {
+  const {playerId} = request.params
+  const matchesQUery = `SELECT match_id,match,year FROM match_details NATURAL JOIN player_details WHERE player_id=${playerId};`
+  const uniqueplayerDetails = await db.all(matchesQUery)
+  response.send(
+    uniqueplayerDetails.map(eachplayer =>
+      ConvertsnaketoCamelCaseofMatchDetails(eachplayer),
+    ),
+  )
+})
+
+module.exports = app
